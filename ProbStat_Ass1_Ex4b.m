f94e0499b24992b6ca336bc5713bdb01232b7734
qvanleersum <quintin@quicknet.nl>
2017-09-28 18:55:42 +0200

    Add files via upload

---
+++ ProbStat_Ass1_Ex4b.m
@@ -0,0 +1,15 @@
+clearvars
+clc
+    answers1 = zeros(20000,1);
+    answers2 = zeros(20000,1);
+    answers3 = zeros(20000,1);
+    for i = 1:20000
+        [E,F,R] = ProbStat_Ass1_Ex4a(250);
+        answers1(i) = E;
+        answers2(i) = F;
+        answers3(i) = R;
+    end
+    
+    disp(sum(sum(answers1)));
+    histogram(answers2);
+    histogram(answers3);
