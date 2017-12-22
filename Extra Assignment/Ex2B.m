b2b9215ec5570f02607362a6d8beb9da2ee403c9
Jari576 <jaribervoets@gmail.com>
2017-12-22 17:15:44 +0100

    ex2b

--- Extra Assignment/Ex2B.m
+++ Extra Assignment/Ex2B.m
@@ -5,4 +5,13 @@ num = xlsread("Extra Assignment\GegevensMeteo-opdracht.xls",'A4:B3656');
 R = num(:,2) > 0;
 p = sum(R) / 3652;
 
-histogram(num(:,2))
+rains = num(:,2);
+%histogram(rains(rains > 0));
+strd = std(rains);
+size(rains(rains > 0));
+rainsmean = sum(rains(rains > 0))/3652;
+L = 1/rainsmean;
+cdfplot(rains)
+hold on
+fplot(@(x) 1 - exp(-L*x), [0,400]);
+legend('Empirical','Theoretical','Location','NW')
